79b7fcf84777cbb1eaaed62eecf78e5cbe17add3
Alexander3006 <ukrpresident2036@gmail.com>
2020-11-14 21:54:33 +0200

    add: machine, disk api

--- server/cmd/server/controllers/controllers.go
+++ server/cmd/server/controllers/controllers.go
@@ -13,9 +13,17 @@ type Controllers struct {
 
 func (c Controllers) Register()  {
 	r := c.router
+
 	//machineHandlers
+	r.RegisterRoute("GET", "/api/machine", r.CreateController(c.getMachineById))
+	r.RegisterRoute("GET", "/api/machine/all", r.CreateController(c.getAllMachines))
 	r.RegisterRoute("POST", "/api/machine/create", r.CreateController(c.createMachine))
+	r.RegisterRoute("POST", "/api/machine/addDisk", r.CreateController(c.addDisk))
+	r.RegisterRoute("DELETE", "/api/machine/delete", r.CreateController(c.deleteMachine))
+	r.RegisterRoute("DELETE", "/api/machine/removeDisk", r.CreateController(c.removeDisk))
 
 	//diskHandlers
-
+	r.RegisterRoute("GET", "/api/disk/all", r.CreateController(c.getAllDisks))
+	r.RegisterRoute("POST", "/api/disk/create", r.CreateController(c.createDisk))
+	r.RegisterRoute("DELETE", "/api/disk/delete", r.CreateController(c.deleteDisk))
 }
